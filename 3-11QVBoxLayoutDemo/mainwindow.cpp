99311130148c456de5177171ae94fa5cfc4ddecf
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-05-25 12:40:34 +0900

    [Upload]:add QHBoxLayout and QVBoxLayout classes explanations

---
+++ 3-11QVBoxLayoutDemo/mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QVBoxLayout>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+//        QWidget * widget = new QWidget;
+
+//        // 1. Define the layout
+//        QVBoxLayout * layout = new QVBoxLayout();
+
+//        // 2. Add things to the layout
+//        layout->addWidget(ui->oneButton);
+//        layout->addWidget(ui->twoButton);
+//        layout->addWidget(ui->threeButton);
+//        layout->addWidget(ui->fourButton);
+//        layout->addWidget(ui->fiveButton);
+
+//        // 3. Set layout to widget
+//        widget->setLayout(layout);
+//        setCentralWidget(widget);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
